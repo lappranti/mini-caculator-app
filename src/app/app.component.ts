f65b6bee2fff2aa4888c65b00a43c0f5e44cf219
lappranti <ndeurys@gmail.com>
2023-03-21 20:35:56 +0000

    Updloading the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'caculator-app';
+export class AppComponent implements OnInit, DoCheck {
+  currentTheme!: string;
+  display: string = '0';
+  previousDisplay: string = '0';
+
+  ngDoCheck(): void {
+    // Vérifiez si la propriété "display" a changé depuis la dernière détection de changement
+    if (this.display !== this.previousDisplay) {
+      // Affichez la nouvelle valeur de "display"
+      let affichage = document.getElementById('input');
+      if (affichage) affichage.innerHTML = this.display;
+      // Mettez à jour la valeur précédente de "display"
+      this.previousDisplay = this.display;
+    }
+  }
+  ngOnInit(): void {
+    this.currentTheme = 'theme1';
+  }
+
+  onKeyClick(input: string) {
+    this.display =
+      this.display == '0' || this.display == 'Error'
+        ? input
+        : this.display + input;
+    console.log(this.display);
+    //this.display = this.calculator(this.display);
+  }
+
+  calculator() {
+    let expression = this.display;
+    // Vérifie que l'expression contient uniquement des chiffres et des opérateurs valides (+, -, *, /)
+    if (!/^[\d+\-*/.()]+$/.test(expression)) {
+      this.display = expression;
+    } else {
+      // Évalue l'expression et retourne le résultat en tant que chaîne de caractères
+      const result = eval(expression);
+      this.display = result.toString();
+    }
+  }
+
+  deleteLastChar() {
+    this.display = this.display.length > 1 ? this.display.slice(0, -1) : '0';
+  }
+
+  onClear() {
+    this.display = '0';
+  }
+
+  selectTheme(theme: string) {
+    this.currentTheme = theme;
+  }
 }
